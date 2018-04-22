8f18ac39bf01543ca9df9e6d272286e105b3cef0
__SyS__ <sreyassreelal@gmail.com>
2018-04-22 20:26:35 +0530

    added torrent search feature

--- bot_main.py
+++ bot_main.py
@@ -5,19 +5,20 @@ from bot_music import Music
 from bot_mp3 import Youtube2Mp3
 from bot_lyrics import Lyrics
 from bot_chat import BotChatAi
+from bot_pirate import Torrent
 
 cosmic = commands.Bot(command_prefix='$')
+
 
 @cosmic.event
 async def on_ready():
     print('Started cosmic...')
-    
-
 
 cosmic.add_cog(Music(cosmic))
 cosmic.add_cog(Youtube2Mp3(cosmic))
 cosmic.add_cog(Lyrics(cosmic))
 cosmic.add_cog(BotChatAi(cosmic))
+cosmic.add_cog(Torrent(cosmic))
 
 f = open('token.txt','r')
 token=f.read().strip()
---
+++ bot_pirate.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import asyncio
+from bs4 import BeautifulSoup
+import requests
+
+class Torrent:
+    def __init__(self,bot):
+        self.bot = bot
+
+    async def search(self,name,channel,user):
+        await self.bot.wait_until_ready()
+        try:
+            res = requests.get("https://thepiratebay-proxylist.org")
+            soup = BeautifulSoup(res.content,'html.parser')
+            proxy = soup.find('td',{'class':'url'})['data-href']
+            payload = proxy+'/s/?q='+name+'&page=0&orderby=99'
+            res = requests.get(payload)
+            soup = BeautifulSoup(res.content,'html.parser')
+            payload = proxy + soup.find('a',{'class':'detLink'})['href']
+            res = requests.get(payload)
+            soup = BeautifulSoup(res.content,'html.parser')
+            magnet = soup.find('a',{'style':"background-image: url('/static/img/icons/icon-magnet.gif');"})['href']
+            about  = soup.find('div',{'class':'nfo'}).text
+        
+            if magnet is not None:
+                await self.bot.send_message(channel,"Got your torrent's magnet link")
+                await self.bot.send_message(channel,"```"+ magnet + "```")
+                if len(about)>10:
+                    await self.bot.send_message(channel,"\n\nTorrent info\n")
+                    await self.bot.send_message(channel,"```" + about[:6000] + "```")
+            else:
+                await self.bot.send_message(channel,"Ehh can't find anything with that name")
+        
+        except Exception as e:
+            await self.bot.send_message(channel,"Eww i think something got me there or what i'm searchin is not right")
+            #await self.bot.send_message(channel,'```'+str(e)+'```')
+            
+
+    @commands.command(pass_context=True)
+    async def searchtor(self,ctx,torrent:str,):
+        await self.bot.say("Lemme look for " + torrent + " torrent ....")
+        await self.bot.loop.create_task(self.search(torrent,ctx.message.channel,ctx.message.author))
+        
+       
