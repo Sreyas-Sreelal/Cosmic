d2d4541e1251dd1e2f5c5fa7c1f73128f296c80c
__SyS__ <sreyassreelal@gmail.com>
2018-05-02 15:18:37 +0530

    added custom help command

--- bot_main.py
+++ bot_main.py
@@ -7,9 +7,10 @@ from bot_mp3 import Youtube2Mp3
 from bot_lyrics import Lyrics
 from bot_chat import BotChatAi
 from bot_pirate import Torrent
+from bot_cmds import CMDS
 
 cosmic = commands.Bot(command_prefix='$')
-
+cosmic.remove_command("help")
 
 @cosmic.event
 async def on_ready():
@@ -20,6 +21,7 @@ cosmic.add_cog(Youtube2Mp3(cosmic))
 cosmic.add_cog(Lyrics(cosmic))
 cosmic.add_cog(BotChatAi(cosmic))
 cosmic.add_cog(Torrent(cosmic))
+cosmic.add_cog(CMDS(cosmic))
 
 f = open('token.txt','r')
 token=f.read().strip()
