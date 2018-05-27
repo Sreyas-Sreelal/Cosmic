70310e5778429cd734f957ea0e84846a8b84fea5
__SyS__ <sreyassreelal@gmail.com>
2018-05-27 11:47:13 +0530

    added handle to terminal

--- bot_log.py
+++ bot_log.py
@@ -1,9 +1,15 @@
 import logging
 
 log = logging.Logger('cosmic')
-handle_file = logging.FileHandler('cosmic_log.txt')
 formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
+
+handle_file = logging.FileHandler('cosmic_log.txt')
 handle_file.setFormatter(formatter)
 log.addHandler(handle_file)
+
+handle_terminal = logging.StreamHandler()
+handle_terminal.setFormatter(formatter)
+log.addHandler(handle_terminal)
+
 log.setLevel(logging.DEBUG)
 
