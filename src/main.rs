5cc5884eeb5838b9092422f4c91a8f1fd1870d7c
SyS <sreyassreelal@gmail.com>
2019-07-15 13:03:07 +0000

    run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use serenity::{
-    model::gateway::Ready,
-    prelude::*,
-};
-use log::{info,error,Metadata,Level,Record};
+use log::{error, info, Level, Metadata, Record};
+use serenity::{model::gateway::Ready, prelude::*};
 
-//Handler 
+//Handler
 //Handles every incoming events in the bot
 struct Handler;
 
 impl EventHandler for Handler {
-    fn ready(&self,_ctx: Context, data: Ready) {
-        info!("Connected to account : {}",data.user.name);
+    fn ready(&self, _ctx: Context, data: Ready) {
+        info!("Connected to account : {}", data.user.name);
     }
 }
 
@@ -34,15 +31,15 @@ impl log::Log for Logger {
 
 static COMSIC_LOGGER: Logger = Logger;
 
-fn main() -> Result<(),log::SetLoggerError> {
+fn main() -> Result<(), log::SetLoggerError> {
     //Initialise log and set trace level
-    log::set_logger(&COMSIC_LOGGER)
-        .map(|()| log::set_max_level(log::LevelFilter::Info))?;
+    log::set_logger(&COMSIC_LOGGER).map(|()| log::set_max_level(log::LevelFilter::Info))?;
 
-    let token = std::env::var("COSMIC_TOKEN").expect("Error cannot fetch token.Make sure environment variable COSMIC_TOKEN is set");
+    let token = std::env::var("COSMIC_TOKEN")
+        .expect("Error cannot fetch token.Make sure environment variable COSMIC_TOKEN is set");
     let mut client = Client::new(&token, Handler).expect("Error Cannot build client object");
     if let Err(error) = client.start() {
-        error!("{}",error);
+        error!("{}", error);
     }
     Ok(())
 }
